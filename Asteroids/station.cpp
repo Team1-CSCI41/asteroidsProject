307702096f9c95c687cc82bb967ab07228b6027a
student <student@S20-04.(none)>
2014-04-23 11:42:21 -0700

    Added the smaller, simpler ship.

--- Asteroids/station.cpp
+++ Asteroids/station.cpp
@@ -31,11 +31,7 @@ extern int   stationRotation = 90;
 qreal sine[360], cosine[360];
 
 
-qreal shipCoord[21] = { +0, +3.125, -3.125, -4.6875, +6.25,
-                        -6.25, -9.375, +12.5, -12.5, +14.0625,
-                        -14.0625, +15.625, -15.625, +18.75, -18.75,
-                        -21.875,  +25, -25, -26.5625, -28.125,
-                        -46.875 };
+
 
 
 /*************************************************************************************/
@@ -69,61 +65,12 @@ void  Station::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, 
     painter->setRenderHint( QPainter::Antialiasing );
   //---------------------------------------------------------------------------------------------------
   //Create Plane
-    painter->setPen( QPen( Qt::blue, 2 ) );
-    painter->drawLine( shipCoord[0],  shipCoord[6],  shipCoord[1],  shipCoord[12]);
-    painter->drawLine( shipCoord[1],  shipCoord[12],  shipCoord[0],  shipCoord[15]);
 
-    cout<<shipCoord[15]<<endl;
-
-    painter->drawLine( shipCoord[0],  shipCoord[6],  shipCoord[2],  shipCoord[12]);
-    painter->drawLine( shipCoord[2],  shipCoord[12],  shipCoord[0],  shipCoord[15]);
-
-  //Right Half
     painter->setPen( QPen( Qt::green, 2 ) );
-    painter->drawLine( shipCoord[1],  shipCoord[2],  shipCoord[0],  shipCoord[2] );
-    painter->drawLine( shipCoord[1],  shipCoord[2],  shipCoord[0],  shipCoord[0] );
-    painter->drawLine( shipCoord[0],  shipCoord[0],  shipCoord[0],  shipCoord[2] );
-    painter->drawLine( shipCoord[1],  shipCoord[2],  shipCoord[4],  shipCoord[2] );
-    painter->drawLine( shipCoord[4],  shipCoord[2],  shipCoord[4],  shipCoord[6] );
-    painter->drawLine( shipCoord[4],  shipCoord[6],  shipCoord[7],  shipCoord[6] );
-    painter->drawLine(  shipCoord[7],  shipCoord[6],  shipCoord[7],  shipCoord[5] );
-    painter->drawLine(  shipCoord[7],  shipCoord[5],  shipCoord[9],  shipCoord[3] );
-    painter->drawLine(  shipCoord[9],  shipCoord[3],  shipCoord[11],  shipCoord[5]);
-    painter->drawLine( shipCoord[11],  shipCoord[5],  shipCoord[11],  shipCoord[6] );
-    painter->drawLine( shipCoord[11],  shipCoord[6],  shipCoord[13],  shipCoord[6] );
-    painter->drawLine( shipCoord[13],  shipCoord[6],  shipCoord[16],  shipCoord[2]);
-    painter->drawLine( shipCoord[16],  shipCoord[2],  shipCoord[16],  shipCoord[19]);
-    painter->drawLine( shipCoord[16],  shipCoord[19],  shipCoord[13],  shipCoord[15]);
-    painter->drawLine( shipCoord[13],  shipCoord[15],  shipCoord[11],  shipCoord[15]);
-    painter->drawLine( shipCoord[11],  shipCoord[15],  shipCoord[11],  shipCoord[18]);
-    painter->drawLine( shipCoord[11],  shipCoord[18],  shipCoord[9],  shipCoord[19] );
-    painter->drawLine( shipCoord[9],  shipCoord[19],  shipCoord[7],  shipCoord[18]);
-    painter->drawLine( shipCoord[7],  shipCoord[18],  shipCoord[7],  shipCoord[15] );
-    painter->drawLine( shipCoord[7],  shipCoord[15],  shipCoord[4],  shipCoord[15]);
-    painter->drawLine( shipCoord[4],  shipCoord[15],  shipCoord[0],  shipCoord[20] );
-
-  //Left half
-    painter->drawLine( shipCoord[2],  shipCoord[2],  shipCoord[0],  shipCoord[2] );
-    painter->drawLine( shipCoord[2],  shipCoord[2],  shipCoord[0],  shipCoord[0] );
-    painter->drawLine( shipCoord[0],  shipCoord[0],  shipCoord[0],  shipCoord[2] );
-    painter->drawLine( shipCoord[2],  shipCoord[2],  shipCoord[5],  shipCoord[2] );
-    painter->drawLine( shipCoord[5],  shipCoord[2],  shipCoord[5],  shipCoord[6] );
-    painter->drawLine( shipCoord[5],  shipCoord[6],  shipCoord[8],  shipCoord[6] );
-    painter->drawLine(  shipCoord[8],  shipCoord[6],  shipCoord[8],  shipCoord[5] );
-    painter->drawLine(  shipCoord[8],  shipCoord[5],  shipCoord[10],  shipCoord[3] );
-    painter->drawLine(  shipCoord[10],  shipCoord[3],  shipCoord[12],  shipCoord[5]);
-    painter->drawLine( shipCoord[12],  shipCoord[5],  shipCoord[12],  shipCoord[6] );
-    painter->drawLine( shipCoord[12],  shipCoord[6],  shipCoord[14],  shipCoord[6] );
-    painter->drawLine( shipCoord[14],  shipCoord[6],  shipCoord[17],  shipCoord[2]);
-    painter->drawLine( shipCoord[17],  shipCoord[2],  shipCoord[17],  shipCoord[19]);
-    painter->drawLine( shipCoord[17],  shipCoord[19],  shipCoord[14],  shipCoord[15]);
-    painter->drawLine( shipCoord[14],  shipCoord[15],  shipCoord[12],  shipCoord[15]);
-    painter->drawLine( shipCoord[12],  shipCoord[15],  shipCoord[12],  shipCoord[18]);
-    painter->drawLine( shipCoord[12],  shipCoord[18], shipCoord[10],  shipCoord[19] );
-    painter->drawLine( shipCoord[10],  shipCoord[19],  shipCoord[8],  shipCoord[18]);
-    painter->drawLine( shipCoord[8],  shipCoord[18],  shipCoord[8],  shipCoord[15] );
-    painter->drawLine( shipCoord[8],  shipCoord[15],  shipCoord[5],  shipCoord[15]);
-    painter->drawLine( shipCoord[5],  shipCoord[15],  shipCoord[0],  shipCoord[20] );
+    painter->drawLine( +10,  +0,  +0,  -5 );
+    painter->drawLine(  +0,  -5,  -10,  +0 );
+    painter->drawLine( -10,  +0,  +0,  -20 );
+    painter->drawLine( +0,  -20 ,  +10,  +0 );
 
 
 }
